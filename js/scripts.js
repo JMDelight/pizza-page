8c55b52f5bc591ab8d42791dcd7f596195851361
Joel Delight <thefencingflutist@gmail.com>
2016-06-17 08:26:41 -0700

    allow you to get the price of the pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
+  this.price;
 };
 
 Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping);
   return this.toppings;
+}
+
+Pizza.prototype.getPrice = function() {
+  this.price = "$10";
+  return this.price;
 }
