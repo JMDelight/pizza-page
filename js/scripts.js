8a38ee23b51214ae99030284c5224f1f7763c9a2
Joel Delight <thefencingflutist@gmail.com>
2016-06-17 08:56:14 -0700

    allow the price of the pizza to be set by the pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
-  this.price;
+  this.price = 8;
 };
 
 Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping);
   return this.toppings;
-}
+};
 
 Pizza.prototype.getPrice = function() {
-  this.price = "$10";
+  if (this.size === "small") {
+    var workingPrice = 8;
+    var priceAdjuster = 0.75;
+  } else if(this.size === "medium") {
+    var workingPrice = 10;
+    var priceAdjuster = 1.35;
+  } else if (this.size === "large") {
+    var workingPrice = 12;
+    var priceAdjuster = 2;
+  } else {
+    console.log("There was an illegal size on the Pizza object.");
+  }
+  this.toppings.forEach(function(topping) {
+    workingPrice += priceAdjuster;
+  });
+  this.price = "$" + workingPrice;
   return this.price;
-}
+};
